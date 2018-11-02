aa62b8b88ec9a361d6e01b1dc2cf2bb6ff9675df
Hunor Karamán <hunix94@gmail.com>
2018-11-02 07:04:18 +0100

    add November to months

--- src/views/feed.js
+++ src/views/feed.js
@@ -61,7 +61,7 @@ function view(state, emit) {
 
 		function date(string) {
 			var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-			var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'December']
+			var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
 			var d = new Date(string)
 			return days[d.getDay()] + ', ' + d.getDate() + '. ' + months[d.getMonth()] + ' ' + d.getFullYear() + '.'
